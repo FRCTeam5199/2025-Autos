44f7843071236b6cafbc382932b000efb61ce7f7
leonardosent <leonardosent@gmail.com>
2025-02-24 20:45:13 -0800

    Added driveAuto

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,14 +4,18 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.DriveSubsystem;
 
 public final class Autos {
-  /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  // Drive forward forever until A-Stopped
+  public static Command driveAuto(DriveSubsystem driveSubsystem) {
+    return new RunCommand(
+        () -> driveSubsystem.drive(
+            0.5, 0, 0,
+            true),
+        driveSubsystem);
   }
 
   private Autos() {
